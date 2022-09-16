5c9cd8497fb23606596e18bd622be2906ae29ea8
PPRomaa <popadinets.roman1@gmail.com>
2022-09-16 16:13:53 +0300

    initial commit

---
+++ src/users/Users.js
@@ -0,0 +1,20 @@
+import {useState} from "react";
+import User from "../users/User";
+
+
+export default function Users(){
+    let [users,setUsers] = useState([]);
+
+    fetch('https://rickandmortyapi.com/api/character')
+        .then(value => value.json())
+        .then(value => {
+            setUsers(value.results);
+        })
+
+    return (
+        <div className={'block'}>
+            {users.map((user,index) => (<User item={user} key={index}/>))}
+        </div>
+    );
+
+}
