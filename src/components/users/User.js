fa46de005834c0e8b95d07e4377eda735ae14ee5
PPRomaa <popadinets.roman1@gmail.com>
2022-09-18 19:22:41 +0300

    initial commit

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,9 @@
-export default function User(props) {
-    let {item: user} = props;
+export default function User({item}) {
 
     return (<div className={'rocketBlock'}>
-        <h2 className={'fmName'}> Mission name - {user.mission_name}</h2>
-        <img src={user.links.mission_patch_small} alt=''></img>
-        <h3 className={'rlName'}> Rocket name - {user.rocket.rocket_name} <br/> Launch year - {user.launch_year}</h3>
+        <h2 className={'fmName'}> Mission name - {item.mission_name}</h2>
+        <img src={item.links.mission_patch_small} alt=''></img>
+        <h3 className={'rlName'}> Rocket name - {item.rocket.rocket_name} <br/> Launch year - {item.launch_year}</h3>
 
     </div>);
 }
